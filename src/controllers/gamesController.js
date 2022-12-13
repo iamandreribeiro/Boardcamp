014d2244fd28efb2cb35e9c032b2f1ca130d29c1
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-12-13 11:38:48 -0300

    feat: games route

--- src/controllers/gamesController.js
+++ src/controllers/gamesController.js
@@ -1,3 +1,25 @@
-export async function postGame(req, res) {}
+import { connectionDB } from "../database/db.js";
 
-export async function getAllGames(req, res) {}
+export async function postGame(req, res) {
+  const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+  try {
+    await connectionDB.query(
+      "INSERT INTO games (name, image, stockTotal, categoryId, pricePerDay) VALUES ($1, $2, $3, $4, $5);",
+      [name, image, stockTotal, categoryId, pricePerDay]
+    );
+
+    res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
+
+export async function getAllGames(req, res) {
+  try {
+    const { rows } = await connectionDB.query("SELECT * FROM games;");
+    res.send(rows);
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+}
--- src/middlewares/gamesValidationMiddleware.js
+++ src/middlewares/gamesValidationMiddleware.js
@@ -1,1 +1,18 @@
-export async function validateGame(req, res, next) {}
+export async function validateGame(req, res, next) {
+  const { name, image, stockTotal, categoryId, pricePerDay } = req.body;
+
+  const validation = postCustomerSchema.validate({
+    name,
+    image,
+    stockTotal,
+    categoryId,
+    pricePerDay,
+  });
+
+  if (validation.error) {
+    const errors = validation.error.details((error) => error.message);
+    res.status(400).send(errors);
+  }
+
+  next();
+}
