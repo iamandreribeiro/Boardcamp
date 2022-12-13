5a4707c2cad107a3266584d42a87a429c9685e6e
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-12-13 10:46:34 -0300

    feat: creating schemas

---
+++ src/models/customerModel.js
@@ -0,0 +1,8 @@
+import Joi from "joi";
+
+export const postCustomerSchema = Joi.object({
+  name: Joi.string().required().min(3),
+  phone: Joi.string().required().min(10).max(11),
+  cpf: Joi.string().required().min(11).max(11),
+  birthday: Joi.string().required().format(["YYYY-MM-DD"]),
+});
---
+++ src/models/rentalModel.js
@@ -0,0 +1,7 @@
+import Joi from "joi";
+
+export const postRentalSchema = Joi.object({
+  customerId: Joi.number().required().min(1),
+  gameId: Joi.number().required().min(1),
+  daysRented: Joi.number().required().min(1),
+});
