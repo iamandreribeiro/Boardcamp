2dee5e909726b5635bc70960d17b4d64ab9eded4
iamandreribeiro <andreribeiroorafael@gmail.com>
2022-12-13 11:32:02 -0300

    fix: app.listen correct port

--- src/server.js
+++ src/server.js
@@ -6,7 +6,6 @@ import customersRoute from "./routes/customersRoute.js";
 import rentalsRoute from "./routes/rentalsRoute.js";
 
 const app = express();
-app.use(cors());
 
 app.use(express.json());
 app.use(cors());
@@ -16,4 +15,4 @@ app.use(gamesRoute);
 app.use(customersRoute);
 app.use(rentalsRoute);
 
-app.listen(process.env.PORT, () => console.log(`Server running in PORT:${process.env.PORT}`));
+app.listen(4000, () => console.log(`Server running in PORT: 4000`));
